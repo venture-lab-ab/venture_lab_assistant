74c2e7f7f4542eba4fdd03fd706aa3aeb4caccfe
hosthans <hannibunni12e@gmail.com>
2021-06-15 14:17:20 +0200

    Updated pubspec.yaml and created new class for Login Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Login.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.purple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Login(),
     );
   }
 }

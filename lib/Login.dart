fc7fab27490355e32747d924af68255f64913e50
hosthans <hannibunni12e@gmail.com>
2021-06-25 18:00:16 +0200

    login - navigationbar philipp/hannes

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,18 +1,69 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:venture_lab_assistant/main.dart';
+import 'dashboard_screen.dart';
 
-class Login extends StatefulWidget {
-  @override
-  _LoginState createState() => _LoginState();
-}
+const users = const {
+  'abc@gmail.com': 'abc',
+  'hunter@gmail.com': 'hunter',
+};
 
-class _LoginState extends State<Login> {
+class LoginScreen extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: 2);
+
+  Future<String?> _authUser(LoginData data) {
+    print('Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'User not exists';
+      }
+      if (users[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      printsth();
+      return null;
+    });
+  }
+
+  Future<String> _recoverPassword(String name) {
+    print('Name: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'User not exists';
+      }
+      return "Huch";
+    });
+  }
+
+  void printsth(){
+    print('Test');
+  }
+
+  static const logopath = 'images/vlogo.png';
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Text("Hallo :)..."),
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("lib/background/img.jpg"),
-                fit: BoxFit.cover)));
+    return FlutterLogin(
+      title: 'VentureApp',
+      logo: logopath,
+      onLogin: _authUser,
+      onSignup: _authUser,
+
+      onSubmitAnimationCompleted: () {
+        print('Navigator');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MyHomePage(title: "Test")),
+        );
+      },
+      onRecoverPassword: _recoverPassword,
+    );
   }
 }
+
+
+
